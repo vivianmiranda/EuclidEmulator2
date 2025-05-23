38e201ccb4c192271374da371dae3abe0ff48d27
Vivian Miranda <vivian.miranda.astro@gmail.com>
2025-05-23 00:25:31 -0400

    found the  bug

--- src/emulator.cxx
+++ src/emulator.cxx
@@ -160,7 +160,6 @@ void EuclidEmulator::compute_nlc(Cosmology csm,
   arma::Col<double>::fixed<this->npcs> pc_weight(arma::fill::zeros);
   for(int ipc=1; ipc<this->npcs+1; ipc++) {
     for(int ic=0; ic<n_coeffs[ipc-1]; ic++) {
-       // assemble PCE to get the PCA weight according to inner sum of eq. 27 in EE2 paper
       double basicfunc = 1.0;
       for(int ipar=0; ipar<this->nindices; ipar++) {
         basicfunc *= univ_legendre(this->pce_multiindex(ic*8+ipar,ipc-1),ipar);
@@ -189,7 +188,7 @@ void EuclidEmulator::compute_nlc(Cosmology csm,
   for(int iz=0; iz<n_redshift; iz++) {
     for(int ik=0; ik<nk; ik++) {
       Bvec[iz][ik] = tmp(iz,ik,0);
-      for(int ipc=1; ipc<this->npcs; ipc++) {
+      for(int ipc=1; ipc<this->npcs+1; ipc++) {
         Bvec[iz][ik] += pc_weight(ipc-1)*tmp(iz,ik,ipc);
       }
     }
